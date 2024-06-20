428b54917ec5cfb3c727e3d56846ad4ce7c9267e
223880 <rafael.segala45@gmail.com>
2024-06-20 09:07:01 -0300

    Fix pair

--- bitswap/pairs.rs
+++ bitswap/pairs.rs
@@ -1,1 +1,13 @@
+use crate::Pair;
+
+pub fn pairs(xs: &[u8]) -> Vec<Pair> {
+    let mut pairs = Vec::new();
+    for i in 0..xs.len() {
+        for j in i + 1..xs.len() {
+            pairs.push(Pair::new(xs[i], xs[j]));
+        }
+    }
+    pairs
+}
+
 
